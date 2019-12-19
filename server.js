44c064e8f50633fbd676b282d7e5ec693b862ee1
Abdu Samaraie <abdusamaraie@gmail.com>
2019-12-19 10:08:47 -0500

    - add mongoose and connect to database - create and retrive a camp throw db

--- server.js
+++ server.js
@@ -1,46 +1,65 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
+//connect to database
+mongoose.connect("mongodb://localhost:27017/yelpcamp_db", {
+  useNewUrlParser: true
+});
+//create schema
+const campSchema = mongoose.Schema({
+  name: String,
+  image: String
+});
 
-//vars
-var campgrounds = [
-  {name:'samlmun creek',
-  image: '/imgs/4522970.png'},
-  {name: 'rock mounten',
-  image:'https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'}
-];
+//add model
+const Campground = mongoose.model("Campground", campSchema);
 
 //Routes
-app.get('/', (req,res)=>{
-  res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 //show campgrounds names and images
-app.get('/camps',(req,res)=>{
-  res.render('camps',{campgrounds:campgrounds});
+app.get("/camps", (req, res) => {
+  //get campgrounds from db
+  Campground.find({}, (err, camps) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("camps", { campgrounds: camps });
+    }
+  });
 });
 
 //post new campground
-app.post('/camps',(req,res)=>{
+app.post("/camps", (req, res) => {
   //get data from from and add to campgrounds array
   const name = req.body.name;
   const image = req.body.image;
-  const newCamp = {name:name,image:image};
+  const newCamp = { name: name, image: image };
   //add to the array
-  campgrounds.push(newCamp);
-  //rerdirect back to camps page
-  res.redirect('camps'); //redirect to the get page
+  //campgrounds.push(newCamp);
+  //create new campground and save to db
+  Campground.create(newCamp, (err, newlyCreated) => {
+    if (err) {
+      console.log(err);
+    } else {
+      //rerdirect back to camps page
+      res.redirect("camps"); //redirect to the get page
+    }
+  });
 });
 
 //create new campground
-app.get('/camps/new',(req,res)=>{
-  res.render('new');
+app.get("/camps/new", (req, res) => {
+  res.render("new");
 });
-app.listen(3000,()=>{
-  console.log('Server is running on port 3000!');
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
