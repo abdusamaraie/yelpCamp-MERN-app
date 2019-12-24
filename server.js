a50f43f68cef8bbe0d5bb4e8bf99d3d33e88a68c
Abdu Samaraie <abdusamaraie@gmail.com>
2019-12-24 09:05:24 -0500

    refactor camps route

--- server.js
+++ server.js
@@ -9,23 +9,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //connect to database
 mongoose.connect("mongodb://localhost:27017/yelpcamp_db", {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 //create schema
 const campSchema = mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 //add model
 const Campground = mongoose.model("Campground", campSchema);
 
+//create new campground
+// Campground.create({
+//   name: "Napolian hill",
+//   image:
+//     "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+//   description: "The is the most beatiful campground in town, with clear sky"
+// });
 //Routes
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-//show campgrounds names and images
+//INDEX - show all campground names and images
 app.get("/camps", (req, res) => {
   //get campgrounds from db
   Campground.find({}, (err, camps) => {
@@ -37,12 +46,13 @@ app.get("/camps", (req, res) => {
   });
 });
 
-//post new campground
+//CREATE - post new campground
 app.post("/camps", (req, res) => {
   //get data from from and add to campgrounds array
   const name = req.body.name;
   const image = req.body.image;
-  const newCamp = { name: name, image: image };
+  const desc = req.body.description;
+  const newCamp = { name: name, image: image, description: desc };
   //add to the array
   //campgrounds.push(newCamp);
   //create new campground and save to db
@@ -56,9 +66,22 @@ app.post("/camps", (req, res) => {
   });
 });
 
-//create new campground
+//NEW - show form to create new campground
 app.get("/camps/new", (req, res) => {
   res.render("new");
+});
+
+//SHOW - one campground
+app.get("/camps/:id/show", (req, res) => {
+  //find the camp by id
+  Campground.findById(req.params.id, (err, foundCamp) => {
+    if (err) {
+      console.log(err);
+    } else {
+      //render templete to show the campground
+      res.render("show", { campground: foundCamp });
+    }
+  });
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
